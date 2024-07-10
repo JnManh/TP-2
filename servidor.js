d6db22bb2b77bacfc15b05f240365a2a45510925
Jonathan M. SIlva <jonathan.manhaesilva@gmail.com>
2024-07-10 10:31:52 -0300

    Atividade 10/07

--- servidor.js
+++ servidor.js
@@ -1,11 +1,23 @@
+const { log } = require('console')
 const express = require('express')
 const app = express()
+const PORT = 8080
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'ejs')
 
-app.get('/', (request, response) => {
-    let num = 50 - 10 * 5
-    response.send(`Cesar, lembro de ${num} da sua matéria :((`)
+app.get('/index', (req, res) => {
+    res.render('index')
 })
 
-app.listen(8080)
+app.post('/resultado', (req, res) => {
+    let massa = parseFloat(req.body.massa)
+    let massaM = parseFloat(req.body.massaM)
+    let conta = massa / massaM
+    let result = `N = ${massa} / ${massaM} = ${conta}`
+    console.log(result);
+    res.render('resultado', {result})
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+})
